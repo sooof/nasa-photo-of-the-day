87c0b291fbbe692bac1262f667d00cce332f2155
sooof <sooof@me.com>
2021-10-07 07:54:18 -0700

    #3

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ export const API_KEY = 'rbKSdCBqYa49RiyHPrJwq6sWw4XmQG7jkgyjOqMh'
 
 // export const API_KEY = 'rbKSdCBqYa49RiyHPrJwq6sWw4XmQG7jkgyjOqMh'
 //https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5
+//${BASE_URL}/planetary/apod?api_key=${API_KEY}&count=5
 //https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
-//https://api.nasa.gov/planetary/apod?api_key=${API_KEY}
-//// console.log(`${BASE_URL}/planetary/apod?api_key=${API_KEY}&count=5`)
+//${BASE_URL}/planetary/apod?api_key=${API_KEY}
+//
